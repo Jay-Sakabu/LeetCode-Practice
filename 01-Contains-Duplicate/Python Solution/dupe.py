a4a24abf333fa6ecac66677cf2444172bbdd7e62
EastAsianInc <js4cap@gmail.com>
2025-08-17 10:40:04 -0500

    Reverted erroneous commit

--- 01-Contains-Duplicate/Python Solution/dupe.py
+++ 01-Contains-Duplicate/Python Solution/dupe.py
@@ -2,7 +2,6 @@
 # Here we take advantage of Python's set data structure, which only stores unique elements
 # Thus, if the length of the set created from the list is different from the length of the list, it means there are duplicates.
 
-my_dictionary = {}
-my_dictionary["example"] = 42
-
-print(f"Value for 'example': {my_dictionary['example']}") #Prints
+class Solution:
+    def containsDuplicate(self, nums):
+        return len(set(nums)) < len(nums)
