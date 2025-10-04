06f28b27312826e122aa536daf451e6a09edb01c
EastAsianInc <js4cap@gmail.com>
2025-10-04 13:31:59 -0500

    Solution 07, hashmap, tests passed

--- 07-Valid-Sudoku/Python Solution/validSudoku.py
+++ 07-Valid-Sudoku/Python Solution/validSudoku.py
@@ -3,21 +3,21 @@ from collections import defaultdict
 # Board will only ever be of size 9x9, if 
 class Solution:
     def validSudoku(self, board):
-        cols = defaultdict(set)
+        columns = defaultdict(set)
         rows = defaultdict(set)
-        squares = defaultdict(set)  # key = (r // 3, c // 3) 
+        squares = defaultdict(set)  # key = (r // 3, c // 3) represents 3x3 sub-grid
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                #  If any element matches in the same row, column, or 3x3 square, return False
+                #  If any element matches in the same row, column, or 3x3 sub-grid, return False
                 if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
+                    board[r][c] in columns[c] or
                     board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                cols[c].add(board[r][c])
+                columns[c].add(board[r][c])
                 rows[r].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
-        # If all checks out, return True
+        # If all checks pass, return True
         return True
